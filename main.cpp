3a3213d7535cf2e5926ffa41b916d8cddf622faf
YoussefFekky <YoussefFekky@users.noreply.github.com>
2021-03-24 02:07:05 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//Made by Youssef Ahmed Hassan on 20/05/2018
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+void ShellSort(T data[], int n)
+{
+    for(int gap = n/2; gap > 0; gap /= 2)
+        for(int i = gap, j; i < n; i++)
+        {
+            T temp = data[i];
+            for(j = i; j >= gap && temp < data[j-gap]; j -= gap)
+                data[j] = data[j-gap];
+            data[j] = temp;
+        }
+}
+
+int main()
+{
+    int arr[] = {5, 2, 7, 1, 9, 11, 3, 0};
+    ShellSort(arr, sizeof(arr)/4);
+    for(int i = 0; i < sizeof(arr)/4; i++)
+        cout << arr[i] << endl;
+    return 0;
+}
